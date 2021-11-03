824fb65e26799e18f378e3e0d7906b96d9aa7de5
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-11-03 20:16:29 -0300

    create constant class and package ui fragments

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.foody
+
+class Constants {
+
+    companion object {
+
+        const val API_KEY = "af04e6121579480eae42172cd67702ff"
+        const val BASE_URL = "https://api.spoonacular.com"
+
+    }
+}
